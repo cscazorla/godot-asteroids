21c80212affcb6e5d743b5aeebad5a46ad5768fd
cscazorla <cscazorla@gmail.com>
2022-02-04 12:43:17 +0100

    Adding an animation to the health power-up dissappearing

--- scenes/PowerupHealth.gd
+++ scenes/PowerupHealth.gd
@@ -4,4 +4,6 @@ func _ready():
 	$DissappearTime.connect("timeout", self, "_on_dissappear_timeout")
 
 func _on_dissappear_timeout():
+	$AnimationPlayer.play("dissappear")
+	yield($AnimationPlayer, "animation_finished")
 	queue_free()
